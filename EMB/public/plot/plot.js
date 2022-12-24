de77729f49df65f2fefbf9337393055232c51c1f
CorpuzKio <62391675+CorpuzKB@users.noreply.github.com>
2022-12-24 20:04:33 +0800

    Removed back forecasting

--- EMB/public/plot/plot.js
+++ EMB/public/plot/plot.js
@@ -5,31 +5,25 @@ async function plot_Predictions(obj, X_predict, X_train, y_train, y_mean, y_cov,
     X_train = await X_train.squeeze().array();
     y_train = await y_train.squeeze().array();
     X_predict = await X_predict.squeeze().array();
-    y_cov = await y_cov.array();
-    y_cov = await y_cov.map(x => x.map(x => x || 1e-9));
-    y_cov = tf.tensor(y_cov);
-
-    y_std = tf.sqrt(obj.getDiag(y_cov, y_cov.shape[0]));
-    y_std.print();
+    let y_std = tf.sqrt(obj.getDiag(y_cov, y_cov.shape[0]));
     let y_UpperCI = await tf.squeeze(y_mean.add(tf.mul(y_std, 2))).array();
     let y_LowerCI = await tf.squeeze(y_mean.sub(tf.mul(y_std, 2))).array();
-
-    console.log(y_UpperCI, y_LowerCI);
-
     y_mean = await y_mean.squeeze().array();
+
     let plot_train = {
         x: X_train,
         y: y_train,
         line: {color: "rgb(0,100,80)"}, 
-        mode: 'markers',
+        mode: 'lines+markers',
         type: 'scatter',
         name: "Reading",
         line: {
-          color: 'rgb(219, 64, 82)',
+          size: 3,  
         },
-        marker: { size: 12 }
-      };
-      
+        marker: { 
+          size: 8,
+          color: 'rgb(219, 64, 82)'
+        }};
       let plot_predict = {
         x: X_predict,
         y: y_mean,
