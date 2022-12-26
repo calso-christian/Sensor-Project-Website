2641a786378c6f1167620c0075c11da4d849e274
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2022-12-26 21:17:22 +0800

    Live Date & Time Added

--- EMB/public/active.js
+++ EMB/public/active.js
@@ -6,20 +6,64 @@ const navLinks = document.querySelectorAll('nav a').forEach(link =>{
 })
 
 
-jQuery(document).ready(function($) {
-    var alterClass = function() {
-      var ww = document.body.clientWidth;
-      if (ww < 749) {
-        $('.first').addClass('order-1');
-        $('.greettarget').addClass('order-2');
-      } else if (ww >= 749) {
-        $('.first').removeClass('order-1');
-        $('.greettarget').removeClass('order-2');
-      };
-    };
-    $(window).resize(function(){
-      alterClass();
-    });
-    //Fire it when the page first loads:
-    alterClass();
-  });
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const meridianElement = document.querySelector(".meridian");
+
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
+  const hours12 = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const isAm = date.getHours() < 12;
+
+  return `${hours12.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
+
+function meridian(date) {
+    const isAm = date.getHours() < 12;
+    return `${isAm ? "AM" : "PM"}`;
+  }
+/**
+ * @param {Date} date
+ */
+function formatDate(date) {
+  const DAYS = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat"
+  ];
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+setInterval(() => {
+  const now = new Date();
+
+  timeElement.textContent = formatTime(now);
+  dateElement.textContent = formatDate(now);
+  meridianElement.textContent = meridian(now);
+}, 200);
