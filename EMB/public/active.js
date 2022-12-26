34a99e7a211eeb6d5d3c83122deed33f67372989
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2022-12-26 20:24:11 +0800

    Bootstrap conversion part 1
    -Remade the non-gauge containers in bootstrap
    -Added static date/time, will apply JS next commit

--- EMB/public/active.js
+++ EMB/public/active.js
@@ -4,3 +4,22 @@ const navLinks = document.querySelectorAll('nav a').forEach(link =>{
         link.classList.add('current');
     }
 })
+
+
+jQuery(document).ready(function($) {
+    var alterClass = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 749) {
+        $('.first').addClass('order-1');
+        $('.greettarget').addClass('order-2');
+      } else if (ww >= 749) {
+        $('.first').removeClass('order-1');
+        $('.greettarget').removeClass('order-2');
+      };
+    };
+    $(window).resize(function(){
+      alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
+  });
