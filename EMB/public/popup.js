ba338369f91550f122fc767c353c0d44e985f803
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-07 21:43:46 +0800

    Working popup but only for one gauge

---
+++ EMB/public/popup.js
@@ -0,0 +1,8 @@
+document.querySelector(".hopen-popup").addEventListener("click", function(){
+    document.querySelector(".hpopup").style.display = "block";
+});
+
+document.querySelector(".hpopup .hclose-btn").addEventListener("click",function(){
+    document.querySelector(".hpopup").style.display = "none";
+})
+
