ff2c042680c613c1e5702477ee449c03235587c3
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-07 23:38:16 +0800

    Added blur and popup info content
    -Also changed bg color can reverse this later on

--- EMB/public/popup.js
+++ EMB/public/popup.js
@@ -1,23 +1,47 @@
 document.querySelector(".hopen-popup").addEventListener("click", function(){
-    document.querySelector(".hpopup").style.display = "block";
+    document.querySelector(".main").style.filter = 'blur(5px)';
+    setTimeout(()=> {
+        document.querySelector(".hpopup").style.display = "block";
+    }
+      ,300);
 });
 
 document.querySelector(".hpopup .hclose-btn").addEventListener("click",function(){
-    document.querySelector(".hpopup").style.display = "none";
-})
+    document.querySelector(".main").style.filter = '';
+    setTimeout(()=> {
+        document.querySelector(".hpopup").style.display = "none";
+    }
+      ,300);
+});
 
 document.querySelector(".topen-popup").addEventListener("click", function(){
-    document.querySelector(".tpopup").style.display = "block";
+    document.querySelector(".main").style.filter = 'blur(5px)';
+    setTimeout(()=> {
+        document.querySelector(".tpopup").style.display = "block";
+    }
+      ,300);
 });
 
 document.querySelector(".tpopup .tclose-btn").addEventListener("click",function(){
-    document.querySelector(".tpopup").style.display = "none";
-})
+    setTimeout(()=> {
+        document.querySelector(".tpopup").style.display = "none";
+    }
+      ,300);
+    document.querySelector(".main").style.filter = '';
+});
 
 document.querySelector(".wopen-popup").addEventListener("click", function(){
-    document.querySelector(".wpopup").style.display = "block";
+    document.querySelector(".main").style.filter = 'blur(5px)';
+    setTimeout(()=> {
+        document.querySelector(".wpopup").style.display = "block";
+    }
+      ,300);
 });
 
 document.querySelector(".wpopup .wclose-btn").addEventListener("click",function(){
-    document.querySelector(".wpopup").style.display = "none";
-})
+    setTimeout(()=> {
+        document.querySelector(".wpopup").style.display = "none";
+    }
+      ,300);
+    document.querySelector(".main").style.filter = '';
+});
