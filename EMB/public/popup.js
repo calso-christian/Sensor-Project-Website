f1611d1a62c86d26fd50cae571d1faf78bb875bd
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-07 22:00:18 +0800

    All gauges have pop up na, will add the other shizz na dapat mangyari

--- EMB/public/popup.js
+++ EMB/public/popup.js
@@ -6,3 +6,18 @@ document.querySelector(".hpopup .hclose-btn").addEventListener("click",function(
     document.querySelector(".hpopup").style.display = "none";
 })
 
+document.querySelector(".topen-popup").addEventListener("click", function(){
+    document.querySelector(".tpopup").style.display = "block";
+});
+
+document.querySelector(".tpopup .tclose-btn").addEventListener("click",function(){
+    document.querySelector(".tpopup").style.display = "none";
+})
+
+document.querySelector(".wopen-popup").addEventListener("click", function(){
+    document.querySelector(".wpopup").style.display = "block";
+});
+
+document.querySelector(".wpopup .wclose-btn").addEventListener("click",function(){
+    document.querySelector(".wpopup").style.display = "none";
+})
