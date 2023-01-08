22d0063d69f3ad3231cef807a5007a31499106e1
CorpuzKio <62391675+CorpuzKB@users.noreply.github.com>
2023-01-08 22:00:17 +0800

    Adjustment

--- EMB/index.js
+++ EMB/index.js
@@ -117,7 +117,7 @@ parser.on('data', async (temp) => {
 io.on('connection', async (socket) => {
     console.log(`Someone connected. ID: ${socket.id}`);
     await Data_reader();
-    await Data_truncate(30);
+    await Data_truncate(900);
     io.sockets.emit('Forecast', [jsonData, 'Temperature']);   
     io.sockets.emit('Forecast', [jsonData, 'Humidity']);
     
