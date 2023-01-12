cf162a12d9864bff8abf905a8d7f0ff3c047a000
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-12 15:53:36 +0800

    Update

--- EMB/public/UI/active.js
+++ EMB/public/UI/active.js
@@ -1,8 +1,5 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link =>{
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('current');
+window.onload = document.querySelectorAll('.nav-link').forEach(link => {
+    if(link.href === window.location.href){
+      link.setAttribute('aria-current', 'page')
     }
-})
-
-
+  })
